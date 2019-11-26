2b9c1638019a136a382dd6c2fc8872cf21d8fc39
Robert Schneider <robmschn@gmail.com>
2019-11-26 15:14:29 -0500

    added loops

--- new_multi.py
+++ new_multi.py
@@ -6,13 +6,15 @@ start = time.perf_counter()
 def do_something(seconds):
     print(f'Sleeping {seconds} second(s)...')
     time.sleep(seconds)
-    return 'Done sleeping.'
+    return f'Done sleeping. {seconds}'
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
-    f1 = executor.submit(do_something, 1)
-    f2 = executor.submit(do_something, 1)
-    print(f1.result())
-    print(f2.result())
+    secs = [5, 4, 3, 2, 1]
+    results = [executor.submit(do_something, sec) for sec in secs]
+
+    for f in concurrent.futures.as_completed(results):
+        print(f.result())
+
 
 # processes = []
 
