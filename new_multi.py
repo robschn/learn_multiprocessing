0d07e8bdd8eb31a8eb1ca2feef52a4ed3ab2adf7
Robert Schneider <robmschn@gmail.com>
2019-11-26 15:06:54 -0500

    added newer multi process method

---
+++ new_multi.py
@@ -0,0 +1,26 @@
+import time
+import concurrent.futures
+
+start = time.perf_counter()
+
+def do_something(seconds):
+    print(f'Sleeping {seconds} second(s)...')
+    time.sleep(seconds)
+    print('Done sleeping.')
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    f1 = executor.submit(do_something, 1)
+
+# processes = []
+
+# for _ in range(10):
+#     p = multiprocessing.Process(target=do_something, args=[1.5])
+#     p.start()
+#     processes.append(p)
+
+# for process in processes:
+#     process.join()
+
+finish = time.perf_counter()
+
+print (f'Finished in {round(finish-start, 2)} second(s)')
